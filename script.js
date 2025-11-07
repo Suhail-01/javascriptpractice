f5fecad43a57e48750bc8f137e31bedfc784bf1a
suhail sayyed <suhailsayyed01@gmail.com>
2025-11-07 22:15:18 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -474,8 +474,38 @@
 
 
 // asker user for any Table of multiplication 
-let n = +prompt("tell us any no.")
-console.log(`the multiplication table of ${n}`);
-for(let i = 1;i<11;i++){
-    console.log(`${n}X${i} = ${n*i}`);
+// let n = +prompt("tell us any no.")
+// console.log(`the multiplication table of ${n}`);
+// for(let i = 1;i<11;i++){
+//     console.log(`${n}X${i} = ${n*i}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write a program using a for loop to print all numbers between 1 and 100,
+// but with these special rules:
+// If a number is divisible by both 3 and 5, print "FizzBuzz".
+// If only divisible by 3, print "Fizz".
+// If only divisible by 5, print "Buzz".
+// Otherwise, print the number itself.
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
