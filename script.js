2a2c03dd4e4e5a3ad12cb509fc913afd448c9683
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-05 21:31:22 +0530

    updated 3 files with minor projects

--- script.js
+++ script.js
@@ -1544,11 +1544,21 @@
   
 
 
-let a , b , c , sum;
-a = 99;
-b = 87;
-c = 879;
-sum = a+b+c;
-console.log(sum);
 
 
+function fibonacci(n) {
+  let a = 0;
+  let b = 1;
+
+  console.log(a); 
+  if (n > 1) console.log(b);
+
+  for (let i = 2; i < n; i++) {
+    let next = a + b;
+    console.log(next);
+    a = b;
+    b = next;
+  }
+}
+
+fibonacci(10);   // prints first 10 Fibonacci numbers
