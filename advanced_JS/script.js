dad09ad1581c79b94ec2f6f3a3ffa218d20d7a68
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-20 20:52:07 +0530

    updated code

--- advanced_JS/script.js
+++ advanced_JS/script.js
@@ -590,14 +590,12 @@
 
 // TRy CAtch Example
 new Promise((resolve) => {
-    resolve(5);
+    resolve(10);
 })
 .then((num) => {
-    return num * 2;
-})
-.then((num) => {
-    return num * 3;
+    num * 2;
 })
 .then((result) => {
     console.log(result);
 });
+
