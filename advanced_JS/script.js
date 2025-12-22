2ef581dbee3d0d23d3b0b03143bc7ee1b0d6af97
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-22 21:00:12 +0530

    updated code

--- advanced_JS/script.js
+++ advanced_JS/script.js
@@ -670,8 +670,8 @@
 // guess
 
 
-const p1 = Promise.resolve("Success");
-const p2 = Promise.reject("Failure");
+const p1 = Promise.reject("Error 1");
+const p2 = Promise.resolve("First success");
 
-Promise.allSettled([p1, p2])
-  .then(results => console.log(results));
+Promise.any([p1, p2])
+  .then(result => console.log(result));
