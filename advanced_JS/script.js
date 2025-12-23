0bb12e0a95ca609f847cdbe36ee3e968efd91db9
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-23 21:17:39 +0530

    updated code

--- advanced_JS/script.js
+++ advanced_JS/script.js
@@ -670,12 +670,9 @@
 // guess
 
 
-function randomPromise() {
-  return new Promise((resolve, reject) => {
-    Math.random() > 0.5 ? resolve("Resolved!") : reject("Rejected!");
-  });
-}
+const p1 = Promise.resolve(1);
+const p2 = Promise.resolve(2);
+const p3 = Promise.resolve(3);
 
-randomPromise()
-  .then(msg => console.log(msg))
-  .catch(err => console.error(err));
+Promise.all([p1, p2, p3])
+  .then(results => console.log(results));
