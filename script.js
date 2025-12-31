8c45235373cd684b50f70cb15986956f13e78acb
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:02:03 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function isEven(num) {
-  return num % 2 === 0;
+function countVowels(str) {
+  return str.match(/[aeiou]/gi)?.length || 0;
 }
