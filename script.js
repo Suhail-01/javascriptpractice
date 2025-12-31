6e72e9e5956633509f2dea912be34890145c66aa
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:08:10 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function getObjectKeys(obj) {
-  return Object.keys(obj);
+function getObjectValues(obj) {
+  return Object.values(obj);
 }
