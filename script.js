7359d35900b73cee39e12c603c7fece5b5a568b0
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:01:01 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,7 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-
-function sumArray(arr) {
-  return arr.reduce((sum, num) => sum + num, 0);
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
 }
