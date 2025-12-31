38296a255307569134e2eeffa659b9ff4cffa211
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:04:12 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function mergeArrays(a, b) {
-  return [...a, ...b];
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
