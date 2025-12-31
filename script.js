d380a31d420cd52e71235a13afb88d90e65b2c78
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:02:16 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function countVowels(str) {
-  return str.match(/[aeiou]/gi)?.length || 0;
+function celsiusToFahrenheit(c) {
+  return (c * 9) / 5 + 32;
 }
