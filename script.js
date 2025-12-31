7f44265481c50d26c7211ca08a7bb0287125a3b0
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:02:32 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function celsiusToFahrenheit(c) {
-  return (c * 9) / 5 + 32;
+function sortArray(arr) {
+  return arr.sort((a, b) => a - b);
 }
