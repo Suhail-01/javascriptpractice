535d48c3ed82ab20eaf9e1605d3f8ddabd25c9ec
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:06:49 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function flattenArray(arr) {
-  return arr.flat();
+function arrayToString(arr) {
+  return arr.join(",");
 }
