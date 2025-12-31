64b7589d1afb31b72fcc733978d65281f38dca59
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:04:25 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function randomBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function countOccurrences(arr, value) {
+  return arr.filter(v => v === value).length;
 }
