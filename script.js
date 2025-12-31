a0bcc336a013d15754749fa45180a0d0d1b5678f
suhail sayyed <suhailsayyed01@gmail.com>
2025-12-31 21:07:01 +0530

    updated some logic and new code as well

--- script.js
+++ script.js
@@ -1728,6 +1728,6 @@
 // let user2 = new user("myname","h@h.in")
 // let user3 = new user("myname","h@h.in")
 // let user4 = new user("myname","h@h.in")
-function arrayToString(arr) {
-  return arr.join(",");
+function getCurrentDateTime() {
+  return new Date();
 }
